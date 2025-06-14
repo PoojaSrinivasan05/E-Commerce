ae9dd33a99b3116b2abadf6e146a35272d41136f
Pooja <poojasrinivasan034@gmail.com>
2025-06-14 13:31:36 +0530

    ProductDetails

--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
 import ProductZoom from "../../components/ProductZoom";
+import Rating from "@mui/material/Rating";
 
 export const ProductDetails =()=>{
     return(
@@ -13,7 +14,7 @@ export const ProductDetails =()=>{
             underline="hover"
             color="inherit"
             href="/"
-            className="link transition"
+            className="link transition !text-[14px]"
           >
             Home
           </Link>
@@ -21,17 +22,44 @@ export const ProductDetails =()=>{
             underline="hover"
             color="inherit"
             href="/"
-            className="link transition"
+            className="link transition !text-[14px]"
           >
             Fashion
+          </Link>
+           <Link
+            underline="hover"
+            color="inherit"
+            className="link transition !text-[14px]"
+          >
+           Apple Watch
           </Link>
         </Breadcrumbs>
       </div>
      </div>
      <section className="bg-white py-5">
-      <div className="container flex gap-4" >
-           <div className="productZoomContainer w-[30%]">
+      <div className="container flex gap-5" >
+           <div className="productZoomContainer w-[40%]">
             <ProductZoom />
+           </div>
+
+           <div className="productContent w-[60%]">
+            <h1 className="text-[28px] font-[600] mb-2">Apple Smart Watch</h1>
+            <div className="flex items-center gap-3">
+              <span className="text-gray-400 text-[20px] ">Brands : <span className="font-[500] text-black opacity-75"></span>Initech space</span>
+
+               <Rating name="size-small" defaultValue={5} size="small" readOnly />
+               <span className="text-[13px] cursor-pointer">Review (5)</span>
+            </div>
+            <div className="flex items-center gap-4 mt-4">
+          <span className="oldPrice line-through text-gray-500 text-[12px]">
+            ₹19,000
+          </span>
+          <span className="price text-primary font-bold text-[18px] ">₹15,000</span>
+        </div>
+        <br />
+
+        <p className="text-[18px]">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+          If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. </p>
            </div>
        </div>
      </section>
--- src/components/ProductZoom/index.jsx
+++ src/components/ProductZoom/index.jsx
@@ -1,40 +1,190 @@
-import React from "react";
+import React, { useState,useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "react-inner-image-zoom/lib/styles.min.css";
 import InnerImageZoom from "react-inner-image-zoom";
-import 'react-inner-image-zoom/lib/styles.min.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
 
 export const ProductZoom = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const zoomSliderBig = useRef();
+  const zoomSliderSml = useRef();
+
+  const goto=(index)=>{
+    setSlideIndex(index);
+    zoomSliderSml.current.swiper.slideTo(index);
+    zoomSliderBig.current.swiper.slideTo(index);
+  }
   return (
     <>
       <div className="flex gap-3">
-        <div className="slider w-[30%]">
-            <Swiper
-                    direction={"vertical"}
-                    slidesPerView={4}
-                    spaceBetween={10}
-                    navigation={true}
-                    pagination={{
-                      clickable: true,
-                    }}
-                    modules={[Navigation]}
-                    className="mySwiper"
-            >
-                <SwiperSlide>
-                    <div  className="item">
-                        <img src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/80-home_default/brown-bear-cushion.jpg"/>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
+        <div className="slider w-[15%] ">
+          <Swiper
+            ref={zoomSliderSml}
+            direction={"vertical"}
+            slidesPerView={5}
+            spaceBetween={0}
+            navigation={true}
+            modules={[Navigation]}
+            className="zoomProductSliderThumbs h-[500px] overflow-hidden"
+          >
+            <SwiperSlide>
+              <div
+                className={`item rounded-md overflow-hidden  cursor-pointer group ${slideIndex===0 ? 'opacity-1' : 'opacity-30'}`}
+                onClick={() => goto(0)}
+              >
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/80-home_default/brown-bear-cushion.jpg"
+                  className="w-full
+                transition-all group-hover:scale-105"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div
+               className={`item rounded-md overflow-hidden  cursor-pointer group ${slideIndex===1 ? 'opacity-1' : 'opacity-30'}`}
+                onClick={() => goto(1)}
+              >
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/37-home_default/the-best-is-yet-to-come-framed-poster.jpg"
+                  className="w-full
+                transition-all group-hover:scale-105"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div
+                className={`item rounded-md overflow-hidden  cursor-pointer group ${slideIndex===2 ? 'opacity-1' : 'opacity-30'}`}
+                onClick={() => goto(2)}
+              >
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/66-home_default/mug-the-adventure-begins.jpg"
+                  className="w-full
+                transition-all group-hover:scale-105"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div
+                className={`item rounded-md overflow-hidden  cursor-pointer group ${slideIndex===3 ? 'opacity-1' : 'opacity-30'}`}
+                onClick={() => goto(3)}
+              >
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/25-home_default/hummingbird-printed-t-shirt.jpg"
+                  className="w-full
+                transition-all group-hover:scale-105"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div
+               className={`item rounded-md overflow-hidden  cursor-pointer group ${slideIndex===4 ? 'opacity-1' : 'opacity-30'}`}
+                onClick={() => goto(4)}
+              >
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/110-home_default/hummingbird-vector-graphics.jpg"
+                  className="w-full
+                transition-all group-hover:scale-105"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div
+               className={`item rounded-md overflow-hidden  cursor-pointer group ${slideIndex===5 ? 'opacity-1' : 'opacity-30'}`}
+                onClick={() => goto(5)}
+              >
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/110-home_default/hummingbird-vector-graphics.jpg"
+                  className="w-full
+                transition-all group-hover:scale-105"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div
+               className={`item rounded-md overflow-hidden  cursor-pointer group ${slideIndex===6 ? 'opacity-1' : 'opacity-30'}`}
+                onClick={() => goto(6)}
+              >
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/110-home_default/hummingbird-vector-graphics.jpg"
+                  className="w-full
+                transition-all group-hover:scale-105"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide></SwiperSlide>
+          </Swiper>
         </div>
-       
-        <div className="zoomContainer w-[70%]">
-        <InnerImageZoom
-        zoomType="hover"
-        zoomScale={1}
-        scr={"https://demos.codezeel.com/prestashop/PRS21/PRS210502/49-large_default/today-is-a-good-day-framed-poster.jpg"}/>
+
+        <div className="zoomContainer w-[85%] h-[600px] overflow-hidden ">
+          <Swiper ref={zoomSliderBig} slidesPerView={1} spaceBetween={0} navigation={false}>
+            <SwiperSlide>
+              <InnerImageZoom
+                zoomType="hover"
+                zoomScale={1}
+                src={
+                  "https://demos.codezeel.com/prestashop/PRS21/PRS210502/32-medium_default/brown-bear-printed-sweater.jpg"
+                }
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <InnerImageZoom
+                zoomType="hover"
+                zoomScale={1}
+                src={
+                  "https://demos.codezeel.com/prestashop/PRS21/PRS210502/32-medium_default/brown-bear-printed-sweater.jpg"
+                }
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <InnerImageZoom
+                zoomType="hover"
+                zoomScale={1}
+                src={
+                  "https://demos.codezeel.com/prestashop/PRS21/PRS210502/32-medium_default/brown-bear-printed-sweater.jpg"
+                }
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <InnerImageZoom
+                zoomType="hover"
+                zoomScale={1}
+                src={
+                  "https://demos.codezeel.com/prestashop/PRS21/PRS210502/32-medium_default/brown-bear-printed-sweater.jpg"
+                }
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <InnerImageZoom
+                zoomType="hover"
+                zoomScale={1}
+                src={
+                  "https://demos.codezeel.com/prestashop/PRS21/PRS210502/32-medium_default/brown-bear-printed-sweater.jpg"
+                }
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <InnerImageZoom
+                zoomType="hover"
+                zoomScale={1}
+                src={
+                  "https://demos.codezeel.com/prestashop/PRS21/PRS210502/32-medium_default/brown-bear-printed-sweater.jpg"
+                }
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <InnerImageZoom
+                zoomType="hover"
+                zoomScale={1}
+                src={
+                  "https://demos.codezeel.com/prestashop/PRS21/PRS210502/32-medium_default/brown-bear-printed-sweater.jpg"
+                }
+              />
+            </SwiperSlide>
+           
+          </Swiper>
         </div>
       </div>
     </>
